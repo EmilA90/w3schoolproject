88a5bf1ce0b1016de98ed23e4c0500051d90826e
EmilA90 <xaosnik_666@mail.ru>
2023-02-22 11:09:17 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navbar } from "./compenents/navbar";
+import { Header } from "./compenents/header";
+import { Content } from "./compenents/content"
+import { About } from "./compenents/about";
+import { Contact } from "./compenents/contact";
+import { Location } from "./compenents/location";
+import { Footer } from "./compenents/footer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Navbar/>
+    <Header/>
+    <Content/>
+    <About/>
+    <Contact/>
+    <Location/>
+    <Footer/>
+    </>
+  )
 }
 
 export default App;
---
+++ src/compenents/location.js
@@ -0,0 +1,8 @@
+export function Location(){
+    return (
+        <div className="w3-container">
+  <img src="https://www.w3schools.com/w3images/map.jpg" className="w3-image" style={{ width: "100%" }} />
+</div>
+
+    )
+}
