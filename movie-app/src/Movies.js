062c63d4180bb8dd586ef88cb6bab1a2127c669c
EmilA90 <xaosnik_666@mail.ru>
2023-03-01 15:03:39 +0400

    first commit

---
+++ movie-app/src/Movies.js
@@ -0,0 +1,137 @@
+// // import React, { useState, useEffect } from 'react';
+
+// // export const Movies = () => {
+// //   const [movies, setMovies] = useState([]);
+// //   const [searchTerm, setSearchTerm] = useState('Avengers');
+
+// //   useEffect(() => {
+// //     fetch(`https://www.omdbapi.com/?apikey=278924d5&s=${searchTerm}&type=movie&page=1&plot=short&r=json`)
+// //       .then((res) => res.json())
+// //       .then((data) => setMovies(data.Search.slice(0, 20)));
+// //   }, [searchTerm]);
+
+// //   return (
+// //     <div className='wrapper'>
+// //       <h1> Movies</h1>
+// //       <div className='search'>
+// //         <input
+// //           type='text'
+// //           placeholder='Search movies...'
+// //           value={searchTerm}
+// //           onChange={(e) => setSearchTerm(e.target.value)}
+// //         />
+// //       </div>
+// //       <div className='card'>
+// //         {movies.map((movie) => (
+// //           <div key={movie.imdbID} className='scard'>
+// //             <h2>{movie.Title}</h2>
+// //             <img src={movie.Poster} />
+// //           </div>
+// //         ))}
+// //       </div>
+// //     </div>
+// //   );
+// // };
+
+
+// import React, { useState, useEffect } from 'react';
+
+// export const Movies = () => {
+//   const [movies, setMovies] = useState([]);
+//   const [searchTerm, setSearchTerm] = useState('Avengers');
+//   const [selectedMovies, setSelectedMovies] = useState([]);
+
+//   useEffect(() => {
+//     fetch(`https://www.omdbapi.com/?apikey=278924d5&s=${searchTerm}&type=movie&page=1&plot=short&r=json`)
+//       .then((res) => res.json())
+//       .then((data) => setMovies(data.Search.slice(0, 20)));
+//   }, [searchTerm]);
+
+//   const handleAddToList = (movie) => {
+//     setSelectedMovies((prevSelectedMovies) => [...prevSelectedMovies, movie]);
+//   };
+
+//   return (
+//     <div className='wrapper'>
+//       <h1> Movies</h1>
+//       <div className='search'>
+//         <input
+//           type='text'
+//           placeholder='Search movies...'
+//           value={searchTerm}
+//           onChange={(e) => setSearchTerm(e.target.value)}
+//         />
+//       </div>
+//       <div className='card'>
+//         {movies.map((movie) => (
+//           <div key={movie.imdbID} className='scard'>
+//           <h2>{movie.Title}</h2>
+//           <img src={movie.Poster} alt={`${movie.Title} poster`} />
+//           <button className='add-to-list' onClick={() => handleAddToList(movie)}>
+//             Add to List
+//           </button>
+//         </div>
+//         ))}
+//       </div>
+//       <div>
+//         <h2>Selected Movies</h2>
+//         <ul>
+//           {selectedMovies.map((movie) => (
+//             <li key={movie.imdbID}>{movie.Title}</li>
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+
+import React, { useState, useEffect } from 'react';
+
+
+export const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('Avengers');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch(`https://www.omdbapi.com/?apikey=278924d5&s=${searchTerm}&type=movie&page=1&plot=short&r=json`)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.Search.slice(0, 20)))
+      .finally(() => setIsLoading(false));
+  }, [searchTerm]);
+
+  const handleAddToList = (movie) => {
+    console.log('Added to list:', movie.Title);
+  };
+
+  return (
+    <div className='wrapper'>
+      <h1> Movies </h1>
+      <div className='search'>
+        <input
+          type='text'
+          placeholder='Search movies...'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      {isLoading ? (
+        <div className='loading'>Loading...</div>
+      ) : (
+        <div className='card'>
+          {movies.map((movie) => (
+            <div key={movie.imdbID} className='scard'>
+              <img src={movie.Poster} alt={movie.Title} />
+              <h2>{movie.Title}</h2>
+              <button className='add-to-list' onClick={() => handleAddToList(movie)}>
+                Add to List
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
